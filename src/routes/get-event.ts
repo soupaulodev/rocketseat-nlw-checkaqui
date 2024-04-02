70ed24102dd1021d86c79e614d3c8559df0cace8
Paulo Marques <paulomarquesexecultivo@gmail.com>
2024-04-02 19:53:59 -0300

    Finalizacao rota get event

---
+++ src/routes/get-event.ts
@@ -0,0 +1,72 @@
+import { FastifyInstance } from "fastify";
+import {
+  ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import z from "zod";
+import { prisma } from "../lib/prisma";
+
+export async function getEvent(app: FastifyInstance) {
+  app.setValidatorCompiler(validatorCompiler);
+  app.setSerializerCompiler(serializerCompiler);
+
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/events/:eventId",
+    {
+      schema: {
+        params: z.object({
+          eventId: z.string().uuid(),
+        }),
+        response: {
+          200: z.object({
+            event: z.object({
+              id: z.string().uuid(),
+              title: z.string(),
+              slug: z.string(),
+              details: z.string().nullable(),
+              maximumAttendees: z.number().int().nullable(),
+              totalAttendees: z.number().int(),
+            }),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { eventId } = request.params;
+
+      const event = await prisma.event.findUnique({
+        select: {
+          id: true,
+          title: true,
+          slug: true,
+          details: true,
+          maximumAttendees: true,
+          _count: {
+            select: {
+              attendees: true,
+            },
+          },
+        },
+        where: {
+          id: eventId,
+        },
+      });
+
+      if (event === null) {
+        throw new Error("Event not found.");
+      }
+
+      return reply.status(200).send({
+        event: {
+          id: event.id,
+          title: event.title,
+          slug: event.slug,
+          details: event.details,
+          maximumAttendees: event.maximumAttendees,
+          totalAttendees: event._count.attendees,
+        },
+      });
+    }
+  );
+}
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import fastify from "fastify";
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
+import { getEvent } from "./routes/get-event";
 
 const PORT = (process.env.PORT || 3333) as number;
 const app = fastify();
 
 app.register(createEvent);
 app.register(registerForEvent);
+app.register(getEvent);
 
 app
   .listen({ port: PORT })
