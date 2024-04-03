a7c47ad9ceaffd5cb0771aec2a05c65b48d40a34
Paulo Marques <paulomarquesexecultivo@gmail.com>
2024-04-03 02:10:06 -0300

    Configuracão inicial de build do frontend e entrega da build pelo backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,27 @@
 import fastify from "fastify";
+import fastifyStatic from "@fastify/static";
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
 import { getAttendeesBadge } from "./routes/get-attendee-badge";
 import { getAllEvent } from "./routes/get-all-event";
+import path from "path";
+
+const app = fastify();
 
 const PORT = (process.env.PORT || 3000) as number;
-const app = fastify();
+const __dirname = path.resolve();
 
 app.register(createEvent, { prefix: "/api" });
 app.register(registerForEvent, { prefix: "/api" });
 app.register(getEvent, { prefix: "/api" });
 app.register(getAllEvent, { prefix: "/api" });
 app.register(getAttendeesBadge, { prefix: "/api" });
+
+app.register(fastifyStatic, {
+  root: path.join(__dirname, "frontend", "dist"),
+  prefix: "/",
+});
 
 app
   .listen({ port: PORT })
