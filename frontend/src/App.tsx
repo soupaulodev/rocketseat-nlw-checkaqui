e514aecaf04334b0454198d9ce9e657eb6bc1d99
Paulo Marques <paulomarquesexecultivo@gmail.com>
2024-04-03 01:42:21 -0300

    Alteracão de tema

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { Header } from "./components/header";
 
 export function App() {
   return (
-    <div className="max-w-[1216px] mx-auto py-5 flex flex-col gap-5">
+    <div className="max-w-[1216px] h-screen mx-auto py-5 flex flex-col gap-5 max-[1300px]:px-6 p-0">
       <Header />
       <AttendeeList />
     </div>
--- frontend/src/components/icon-button.tsx
+++ frontend/src/components/icon-button.tsx
@@ -10,12 +10,16 @@ export function IconButton({ transparent, ...props }: IconButtonProps) {
     <button
       {...props}
       className={twMerge(
-        "border border-white/10 rounded-md p-1.5",
-        transparent ? "bg-orange-900/90" : "bg-orange-900/70",
+        "border border-white/10 rounded-md p-1.5 duration-200",
+        transparent
+          ? "bg-lime-700/90 hover:bg-lime-600/90"
+          : "bg-lime-700/70 hover:bg-lime-600/70",
         props.disabled ? "opacity-50" : null
       )}
     />
   );
 }
 
-{/* Se tiver na primeira página: deixa transparente os dois primeiros botões, se tiver na última: os dois últimos */}
+{
+  /* Se tiver na primeira página: deixa transparente os dois primeiros botões, se tiver na última: os dois últimos */
+}
