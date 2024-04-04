4b700546749c33e710c709dc5440fe17f80a07a4
Paulo Marques <paulomarquesexecultivo@gmail.com>
2024-04-04 00:45:31 -0300

    Correcão e remocao de resquicios do monorepo

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import fastify from "fastify";
-import fastifyStatic from "@fastify/static";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUI from "@fastify/swagger-ui";
 import { createEvent } from "./routes/create-event";
@@ -7,7 +6,6 @@ import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
 import { getAttendeesBadge } from "./routes/get-attendee-badge";
 import { getAllEvent } from "./routes/get-all-event";
-import path from "path";
 import { checkIn } from "./routes/check-in";
 import { getEventAttendees } from "./routes/get-event-attendees";
 import {
@@ -26,7 +24,6 @@ app.register(fastifyCors, {
 
 const PORT = (process.env.PORT || 3000) as number;
 const HOST = (process.env.HOST || "0.0.0.0") as string;
-const __dirname = path.resolve();
 
 app.register(fastifySwagger, swaggerConfig);
 app.register(fastifySwaggerUI, {
@@ -43,11 +40,6 @@ app.register(getAllEvent, { prefix: "/api" });
 app.register(getAttendeesBadge, { prefix: "/api" });
 app.register(getEventAttendees, { prefix: "/api" });
 app.register(checkIn, { prefix: "/api" });
-
-app.register(fastifyStatic, {
-  root: path.join(__dirname, "frontend", "dist"),
-  prefix: "/",
-});
 
 app.setErrorHandler(errorHandler);
 
