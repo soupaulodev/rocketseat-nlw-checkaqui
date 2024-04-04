8592c6aca254e923e94a3d319a890e7c70eb85c4
Paulo Marques <paulomarquesexecultivo@gmail.com>
2024-04-04 00:01:00 -0300

    Correcão de uso incorreto do setValidatorCompiler e setSerializerCompiler

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,7 @@
 import fastify from "fastify";
 import fastifyStatic from "@fastify/static";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
@@ -8,11 +10,36 @@ import { getAllEvent } from "./routes/get-all-event";
 import path from "path";
 import { checkIn } from "./routes/check-in";
 import { getEventAttendees } from "./routes/get-event-attendees";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  jsonSchemaTransform,
+} from "fastify-type-provider-zod";
 
 const app = fastify();
 
 const PORT = (process.env.PORT || 3000) as number;
 const __dirname = path.resolve();
+
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ["application/json"],
+    produces: ["application/json"],
+    info: {
+      title: "Check-in API",
+      description: "Especificações do CheckAqui API",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUI, {
+  routePrefix: "/docs",
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent, { prefix: "/api" });
 app.register(registerForEvent, { prefix: "/api" });
